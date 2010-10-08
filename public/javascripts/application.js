5d47211f87053c87126a2b6ed480771d39a0ec69
Yossapas <lucifer_ap@hotmail.com>
2010-10-08 04:12:18 -0700

    back up before lightbox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  
+  test();
+  
+});
+
+
+function test(){
+  $("#test").html("<b>It's work!</b>");
+}
